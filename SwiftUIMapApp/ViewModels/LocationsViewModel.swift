098c4d9e071657e4258356e1118d33ae83f44b9a
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-04-01 17:12:44 +0900

    feat: done lecture 03

---
+++ SwiftUIMapApp/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  LocationsViewModel.swift
+//  SwiftUIMapApp
+//
+//  Created by Wataru Miyakoshi on 2024/04/01.
+//
+
+import Foundation
+import MapKit
+
+@MainActor
+final class LocationsViewModel: ObservableObject {
+    @Published var locations: [Location]
+    
+    /// 現在のロケーション
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapLocation(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1
+    )
+
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapLocation(location: locations.first!)
+    }
+    
+    private func updateMapLocation(location: Location) {
+        mapRegion = MKCoordinateRegion(
+            center: location.coordinates,
+            span: mapSpan
+        )
+    }
+}
