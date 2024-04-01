50e7fd0643012acfc914c4f0349334bead2de314
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-04-01 17:47:51 +0900

    feat: done lecture_04

--- SwiftUIMapApp/Models/Location.swift
+++ SwiftUIMapApp/Models/Location.swift
@@ -8,7 +8,7 @@
 import Foundation
 import MapKit
 
-struct Location: Identifiable {
+struct Location: Identifiable, Equatable {
     let name: String
     let cityName: String
     // MapKitを使用するのであれば、座標はCLLocationCoordinate2Dを使用する
@@ -19,5 +19,9 @@ struct Location: Identifiable {
     
     var id: String {
         name + cityName
+    }
+    
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
     }
 }
--- SwiftUIMapApp/Views/LocationsView.swift
+++ SwiftUIMapApp/Views/LocationsView.swift
@@ -15,7 +15,41 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $viewModel.mapRegion)
                 .ignoresSafeArea()
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                Spacer()
+            }
         }
+    }
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Button(action: viewModel.toggleLocationList, label: {
+                Text(viewModel.mapLocation.name + ", " + viewModel.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundStyle(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: viewModel.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundStyle(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: viewModel.isShowingLocationList ? 180 : 0))
+                    }
+            })
+            if viewModel.isShowingLocationList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
 
