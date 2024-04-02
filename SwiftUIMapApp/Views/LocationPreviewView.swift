575ade9ba648da79ec28c90142712fbc35e9b68e
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-04-02 10:39:53 +0900

    feat: done lecture_05

---
+++ SwiftUIMapApp/Views/LocationPreviewView.swift
@@ -0,0 +1,89 @@
+//
+//  LocationPreviewView.swift
+//  SwiftUIMapApp
+//
+//  Created by Wataru Miyakoshi on 2024/04/02.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    @EnvironmentObject private var viewModel: LocationsViewModel
+    let location: Location
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            VStack(alignment: .leading, spacing: 16) {
+                if let imageName = location.imageNames.first {
+                    imageSection(imageName: imageName)
+                }
+                titleSection
+            }
+            VStack(spacing: 8) {
+                learnMoreButton
+                nextButton
+            }
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+                .offset(y: 65)
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.green
+        LocationPreviewView(location: LocationsDataService.locations.first!)
+            .environmentObject(LocationsViewModel())
+    }
+}
+
+extension LocationPreviewView {
+    private func imageSection(imageName: String) -> some View {
+        ZStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .padding(6)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(location.name)
+                .font(.title2.bold())
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreButton: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("Learn more")
+                .font(.headline)
+                .frame(width: 125, height: 30)
+        })
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button(action: {
+            viewModel.nextButtonPressed()
+        }, label: {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 30)
+        })
+        .buttonStyle(.bordered)
+    }
+}
